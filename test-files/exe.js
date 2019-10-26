f380c2d7752e5b54278541c565e49eb81bd81bc8
john <johnathanchewy@gmail.com>
2019-10-26 16:40:55 -0700

    removed testing files

--- test-files/exe.js
+++
@@ -1,7 +0,0 @@
-const { exec, spawn } = require('child_process');
-
-cmdStr = './adhoc-network test 123';
-exec(cmdStr, (err, data) => {
-    console.log(err);
-    console.log(data.toString());
-});
