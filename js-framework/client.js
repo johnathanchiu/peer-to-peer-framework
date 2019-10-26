47f70ea9f746a7f8ade7ae3cf4faf02f83f6b1f6
john <johnathanchewy@gmail.com>
2019-10-26 16:32:43 -0700

    finished js libraries, testing for bugs

--- js-framework/client.js
+++ js-framework/client.js
@@ -1,15 +1,53 @@
 const net = require('net');
+var wifi = require('node-wifi');
 
-var client = net.Socket();
-client.connect(1337, '127.0.0.1', function() {
-	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
-});
+function Client(ssid, password) {
+	wifi.init({ iface: null });
+	wifi.connect({ ssid: ssid, password: pass }, function (err) {
+	    if (err) {
+	        console.log(err);
+	    }
+	});
+}
 
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-});
+Client.prototype.onConn = function (socket, ip, connHandler=null) {
+	this.client = net.Socket();
+	if (connHandler) {
+		this.client.connect(socket, ip, connHandler);
+	} else {
+		this.client.connect(socket, ip, function () {
+			console.log("Connection established!");
+		});
+	}
+}
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+Client.prototype.write = function (data, writeHandler=null) {
+	var success = false;
+    try {
+        if (writeHandler) {
+            success = this.client.write(data, writeHandler);
+        } else {
+            success = this.client.write(data);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    if (!successs) {
+        throw "Unable to successfully write data to Server.";
+    }
+}
+
+Client.prototype.setDataHandler = function (recHandler) {
+	this.client.on('data', recHandler);
+}
+
+Client.prototype.setCloseHandler = function (closeHandler) {
+	this.client.on('close', closeHandler);
+	cmdStr = './adhoc-kill';
+	exec(cmdStr, (err, stdout, stderr) => {
+		if (err) {
+			console.log(stderr);
+		}
+	});
+
+}
--- js-framework/server.js
+++ js-framework/server.js
@@ -1,19 +1,89 @@
-const net = require('net')
+const os = require('os');
+const net = require('net');
 
-var server = net.createServer();
 
-server.on('connection', function(c) {
-    console.log("New Connection!");
-    c.write("Hello!");
-});
+const { exec, spawn } = require('child_process');
 
-server.on('data', function(data) {
-	console.log('Received: ' + data);
-});
 
-server.on('close', function(data) {
-    console.log('');
-});
+function Server(type, socket, ssid=null, password=null) {
+    if (type == 'P2P' && ) {
+        if (process.platform != 'darwin') {
+            throw "This os cannot host as server.";
+        }
+        if (ssid == null && password == null) {
+            throw "No name/password provided for ad-hoc network.";
+        }
+        cmdStr = './adhoc-network ' + ssid + ' ' + password;
+        exec(cmdStr, (err, stdout, stderr) => {
+            if (err) {
+                console.log(stderr);
+            }
+        });
+    }
+    this.server = net.createServer();
+    this.sock = socket;
+}
 
-// server.listen(1337, '127.0.0.1');
-server.listen(1337, '169.254.146.198');
+Server.prototype.listen = function () {
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
+    }
+    if (addresses.length < 1) {
+        throw "Failed to find ip for server.";
+    }
+    this.ip = addresses[0];
+    this.server.listen(this.sock, this.ip);
+    this.server.on('connection');
+}
+
+Server.prototype.setConnHandler = function (connHandler) {
+    this.server.on('connection', connHandler);
+}
+
+Server.prototype.destroy = function (closeHandler=null) {
+    if (this.server.getConnections() > 0) {
+        throw "Cannot close server til all clients are removed.";
+    }
+    if (closeHandler === null) {
+        this.server.on('close', function () {
+            cmdStr = './adhoc-kill';
+            exec(cmdStr, (err, stdout, stderr) => {
+                if (err) {
+                    console.log(stderr);
+                }
+            });
+            exit();
+        }
+    }
+    this.server.on('close', closeHandler);
+    cmdStr = './adhoc-kill';
+    exec(cmdStr, (err, stdout, stderr) => {
+        if (err) {
+            console.log(stderr);
+        }
+    });
+    exit();
+}
+
+Server.prototype.write = function (data, writeHandler=null) {
+    var success = false;
+    try {
+        if (writeHandler) {
+            success = this.server.write(data, writeHandler);
+        } else {
+            success = this.server.write(data);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    if (!successs) {
+        throw "Unable to successfully write data to clients.";
+    }
+}
