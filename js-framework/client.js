ffdae4f37fafac1452da3b8332f0a545b5f61c88
john <johnathanchewy@gmail.com>
2019-10-27 02:29:21 -0700

    finished testing, cleaned up bugs

--- js-framework/client.js
+++ js-framework/client.js
@@ -1,7 +1,7 @@
 const net = require('net');
 var wifi = require('node-wifi');
 
-function Client(ssid, password) {
+function Client(ssid, pass) {
 	wifi.init({ iface: null });
 	wifi.connect({ ssid: ssid, password: pass }, function (err) {
 	    if (err) {
@@ -10,7 +10,7 @@ function Client(ssid, password) {
 	});
 }
 
-Client.prototype.onConn = function (socket, ip, connHandler=null) {
+Client.prototype.createConn = function (socket, ip, connHandler=null) {
 	this.client = net.Socket();
 	if (connHandler) {
 		this.client.connect(socket, ip, connHandler);
@@ -24,11 +24,7 @@ Client.prototype.onConn = function (socket, ip, connHandler=null) {
 Client.prototype.write = function (data, writeHandler=null) {
 	var success = false;
     try {
-        if (writeHandler) {
-            success = this.client.write(data, writeHandler);
-        } else {
-            success = this.client.write(data);
-        }
+        success = this.client.write(data, writeHandler);
     } catch (e) {
         console.log(e);
     }
@@ -41,14 +37,33 @@ Client.prototype.setDataHandler = function (recHandler) {
 	this.client.on('data', recHandler);
 }
 
-Client.prototype.setCloseHandler = function (closeHandler) {
-	this.client.on('close', closeHandler);
+Client.prototype.setCloseHandler = function (closeHandler=null) {
+	this.client.on('close', function () {
+        if (closeHandler != null) {
+            closeHandler();
+        }
+    });
+}
+
+Client.prototype.destroy = function (endAll=false) {
+	this.client.destroy();
 	cmdStr = './adhoc-kill';
 	exec(cmdStr, (err, stdout, stderr) => {
 		if (err) {
 			console.log(stderr);
 		}
 	});
+	if (endAll) {
+		process.exit();
+	}
+}
+
+Client.prototype.invokeKillHandler = function () {
+    var self = this;
+    process.on('SIGINT', function() {
+        console.log("Closing socket.");
+        self.destroy(endAll=true);
+    });
 }
 
 module.exports = Client;
